748bbe7d0d62b9a0f81f014c2e278b38a0a8e87d
troy.johnson <troy.johnson57@gmail.com>
2020-12-03 10:52:43 -0700

    initial commit

---
+++ packages/details/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Details from "./Details";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Details />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./index.css";
+import Home from "@tjohnson/mf-example-home";
+import Details from "@tjohnson/mf-example-details";
+import Search from "@tjohnson/mf-example-details";
+import Settings from "@tjohnson/mf-example-settings";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+          <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/details">Details</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/details" component={Details} />
+          <Route path="/search" component={Search} />
+          <Route path="/settings" component={Settings} />
+        </Switch>
+      </div>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
